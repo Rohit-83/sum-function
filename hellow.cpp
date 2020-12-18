0173b89987ea541c0a9e481ae58588ce8a72023a
Rohit Kumar <rk5744081@gmail.com>
2020-12-18 16:02:44 +0530

    starting

---
+++ hellow.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+ {
+     int a,b;
+     cout<<"enter the value of a:";
+     cin>>a;
+     cout<<"enter the value of b:";
+     cin>>b;
+     int sum;
+     sum=a+b;
+     cout<<"sum of a and b is:"<<sum;
+
+    
+    return 0;
+ }  
